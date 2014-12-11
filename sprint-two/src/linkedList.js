82219c87b74a165a5aae0f7a43096cfbe41e49da
chikeichan <chikeichan@gmail.com>
2014-12-11 11:24:49 -0800

    completed tree

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,42 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var newTail = Node(value);
+    if(list.head === null){
+      list.head = newTail;
+    }
+    if(list.tail === null){
+      list.tail = newTail;
+    } else {
+      //add next node to last.next
+      list.tail.next = newTail;
+      //set tail to new node
+      list.tail = newTail;
+    }
+
   };
 
   list.removeHead = function(){
+    var lastHead = list.head.value;
+    list.head = list.head.next;
+    return lastHead;
   };
 
   list.contains = function(target){
+    var inspect = list.head;
+    var search = function(inspect){
+      if( inspect.value === target ){
+        return true;
+      } else {
+        inspect = inspect.next;
+        if(inspect===null){
+          return false;
+        }
+        return search(inspect);
+      }
+    }
+    return search(inspect);
+
   };
 
   return list;
