31c863363c944051ec4ac0d4f0221df393009c37
chikeichan <chikeichan@gmail.com>
2014-12-11 11:25:38 -0800

    completed tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,7 +3,8 @@ var Tree = function(value){
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  _.extend(newTree,treeMethods);
+  newTree.children = [];  // fix me
 
   return newTree;
 };
@@ -15,11 +16,33 @@ var Tree = function(value){
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target){
+  //declare result to be false;
+  var result = false;
+  var search = function(tree){
+    //if value equal targrt
+    if( tree.value === target ){
+      //setting result to be true, break
+      result = true;
+      return;
+    }
+    if( tree.children.length > 0 ){
+      //if tree children length is greater than 0
+      for( var i = 0; i<tree.children.length; i++){
+        //run search recursively
+        search(tree.children[i]);
+      }
+    }
+  }
 
+  search(this);
+
+
+  //return result
+  return result;
 };
 
 
