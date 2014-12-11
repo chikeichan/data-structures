8a706b74f0cbc3654e23cea77c12b90d5278a8ad
chikeichan <chikeichan@localhost.(none)>
2014-12-10 22:42:15 -0800

    completed linkedlist

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,15 +2,42 @@ var LinkedList = function(){
   var list = {};
   list.head = null;
   list.tail = null;
+ 
 
   list.addToTail = function(value){
+    var linkedListNode = Node(value);
+    if(!this.head && !this.tail){
+      this.head = linkedListNode;
+      this.tail = linkedListNode;
+    } else {
+      this.tail.next = linkedListNode;
+      this.tail = linkedListNode;
+    }
   };
 
   list.removeHead = function(){
+    var removed = this.head.value;
+    this.head = this.head.next;
+    return removed;
   };
 
   list.contains = function(target){
+    var findNext = function(node){
+      console.log(node.value === target);
+      if(node.value === target){
+        return true;
+      }
+      if(node.next!==null){
+        return findNext(node.next);
+      }
+      return false;
+    }
+
+    return findNext(this.head);
+
   };
+
+
 
   return list;
 };
