dd00a7f77abc969a7b75834f9ec63d0106377e40
chikeichan <chikeichan@gmail.com>
2014-12-12 12:32:26 -0800

    Partially done with BSTREE;

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,47 @@
 var BinarySearchTree = function(value){
+  var binarysearchtree = {};
+
+  _.extend(binarysearchtree, methods);
+  //.left properties
+  binarysearchtree.left = null;
+  //.right properties
+  binarysearchtree.right = null;
+  binarysearchtree.value = value;
+
+  return binarysearchtree;
+};
+
+var methods = {};
+methods.insert = function(value){
+  //if value > this.value
+  if(value > this.value){
+    // if this.right is null
+    if(!this.right){
+      // this.right is Tree(value);
+      this.right = BinarySearchTree(value);
+    } else {
+      // run insert recursively on this.right;
+      this.right.insert(value);
+    }
+  }
+  //if value < this.value
+  if(value < this.value){
+    // if this.left is null
+    if(!this.left){
+      // this.left is Tree(value);
+      this.left = BinarySearchTree(value);
+    } else {
+      // run insert recursively on this.left;
+      this.left.insert(value);
+    }
+  }
+};
+
+methods.contains = function(){
+
+};
+methods.depthFirstLog = function(){
+
 };
 
 
--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -4,24 +4,46 @@ var HashTable = function(){
 };
 
 HashTable.prototype.insert = function(k, v){
+
   var i = getIndexBelowMaxForKey(k, this._limit);
-  console.log(i, this._storage.get(i));
-  if(!!this._storage.get(i)){
-    i++;
+  //create a tuple with [k, v]
+  var tup = [k, v];
+  //retrieve the bucket at index i
+  //if no bucket
+  if(this._storage.get(i) === undefined){
+    //make bucket
+    this._storage.set(i, []);
   }
-  console.log(i, this._storage.get(i));
-  this._storage.set(i,v);
+
+  this._storage.get(i).push(tup);
 };
 
 HashTable.prototype.retrieve = function(k){
-  var i = getIndexBelowMaxForKey(k, this._limit);
 
-  return this._storage.get(i);
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  //get bucket
+  var bucket = this._storage.get(i);
+  //iterate inside bucket
+  for(var key = 0;key < bucket.length;key++){
+    if (bucket[key][0] === k){
+      return bucket[key][1];
+    }
+  }
+  return null;
 };
 
 HashTable.prototype.remove = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.set(i,null);
+  //get bucket
+  var bucket = this._storage.get(i);
+  //iterate bucket
+  for(var key = 0; key < bucket.length;key++){
+    //if tuple[0] === k
+    if(bucket[key][0] === k){
+      //splice bucket[tuple key, 1]
+      bucket.splice(key, 1)
+    }
+  }
 };
 
 
